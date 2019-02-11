cb34ee61fe6f86106a8488daff423edc5fdb237d
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-02-11 13:52:34 -1000

    Edit photo content

--- routes/gallery.js
+++ routes/gallery.js
@@ -24,20 +24,40 @@ router.get('/new', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // had to subtract 3 from id instead of 1 for some reason...
-  const databaseId = req.params.id - 3;
+  let id = req.params.id;
 
   knex('gallery')
     .select('id', 'author', 'link', 'description', 'created_at', 'updated_at')
+    .where('id', id)
     .then((selection) => {
-      const getFeatured = selection[databaseId];
-      const withoutFeatured = selection.filter(x => { return selection.indexOf(x) !== databaseId });
-      const getListings = withoutFeatured.splice(0, 3);
-
-      entries.featured = getFeatured;
-      entries.listings = getListings;
-
+      entries.featured = selection[0];
+      return entries;
+    })
+    .then((entries) => {
+      return knex('gallery')
+        .select('id', 'author', 'link', 'description', 'created_at', 'updated_at')
+        .whereNot('id', id)
+        .then((selection) => {
+          entries.listings = selection;
+          return entries;
+        });
+    })
+    .then((entries) => {
       res.render('templates/detail', entries);
+    });
+});
+
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id;
+
+  knex('gallery')
+    .select('id', 'author', 'link', 'description', 'updated_at')
+    .where('id', id)
+    .then((selection) => {
+      console.log(selection);
+
+      entries.featured = selection[0];
+      res.render('templates/edit', entries);
     });
 });
 
@@ -54,6 +74,30 @@ router.post('/post', validation.authenticate, (req, res) => {
     .then((user) => {
       console.log(`USER IS:`); console.log(user);
       res.redirect('/');
+    });
+});
+
+router.put('/:id', (req, res) => {
+  console.log('HIT');
+  const id = req.params.id;
+  const data = req.body;
+
+  console.log('BOOKSHELF');
+  Gallery
+    .where('id', id)
+    .fetch()
+    .then((fetch) => {
+      return fetch
+        .save({
+          author: data.author,
+          link: data.link,
+          description: data.description,
+          updated_at : new Date()
+        });
+    })
+    .then((fetch) => {
+      console.log(fetch)
+      res.redirect(`/gallery/${id}`);
     });
 });
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
@@ -22,6 +23,7 @@ const SESSION_SECRET = process.env.SESSION_SECRET || 'keyboard cat';
 
 const app = express();
 
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
